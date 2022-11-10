fad9c493b8911736e6dc501512730f4d6481114f
Acheberto <116881496+Acheberto@users.noreply.github.com>
2022-11-10 20:05:56 +0100

    first part
    operations (+-%)

---
+++ practice 1.py
@@ -0,0 +1,22 @@
+s1 = 1;
+s2 = 2;
+x = s1;
+print(s1 + s2)
+
+print(s1 - s2)
+
+print(s1 ** s2)
+
+x += 1;
+
+print(x);
+
+x -= 2;
+
+print(x);
+
+print(s1 % s2);
+
+print(s1 // s2);
+
+print(s1 / s2);
