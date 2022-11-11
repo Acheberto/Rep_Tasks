b47118c488db0c81bcbfd4a68caf9da7d165d739
Acheberto <116881496+Acheberto@users.noreply.github.com>
2022-11-11 08:51:35 +0100

    Create ex4.py
    version antigua

---
+++ ex4.py
@@ -0,0 +1,17 @@
+nota = input ('Quina nota tens?');
+nota = int (nota);
+
+if nota <= 4:
+     print ("Suspes");
+elif nota >= 5:
+     print ("Bé");
+elif nota >= 6:
+     print ("Notable");
+elif nota > 8:
+     print ("Exellent");
+
+
+
+
+
+
